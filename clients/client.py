f0ee6c4e884437e80c075ad4b279c0e16cca907b
tenyue <tenyue@users.noreply.github.com>
2016-12-06 14:45:14 +0800

    update: 不知道这样计算对不对，多网卡的时候流量相加

--- clients/client.py
+++ clients/client.py
@@ -109,8 +109,8 @@ class Traffic:
 		return avgrx, avgtx
 
 def liuliang():
-    NET_IN = '0'
-    NET_OUT = '0'
+    NET_IN = 0
+    NET_OUT = 0
     with open('/proc/net/dev') as f:
         for line in f.readlines():
             netinfo = re.findall('([^\s]+):[\s]{0,}(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)', line)
@@ -118,9 +118,9 @@ def liuliang():
                 if netinfo[0][0] == 'lo' or 'tun' in netinfo[0][0] or netinfo[0][1]=='0' or netinfo[0][9]=='0':
                     continue
                 else:
-                    NET_IN = netinfo[0][1]
-                    NET_OUT = netinfo[0][9]
-    return int(NET_IN), int(NET_OUT)
+                    NET_IN += int(netinfo[0][1])
+                    NET_OUT += int(netinfo[0][9])
+    return NET_IN, NET_OUT
 
 def get_network(ip_version):
 	if(ip_version == 4):
