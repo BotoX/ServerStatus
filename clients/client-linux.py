671101e7eca15c801c9a8d0bd9f2d32ce6d52dae
tenyue <tenyue@users.noreply.github.com>
2017-08-17 10:08:47 +0800

    Update client-linux.py
    python2.6 不在支持

--- clients/client-linux.py
+++ clients/client-linux.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Update by : https://github.com/tenyue/ServerStatus
-# 支持Python版本：2.6 to 3.5
+# 支持Python版本：2.7 to 3.5
 # 支持操作系统： Linux, OSX, FreeBSD, OpenBSD and NetBSD, both 32-bit and 64-bit architectures
 # 时间: 20170807
 
