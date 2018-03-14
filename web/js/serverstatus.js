aaf00a60cda6904646d635fcabb9a4cafca60bf2
ubuntu <ubuntu@ubuntu.com>
2018-03-14 12:06:45 +0800

    最新的更新和调试

--- web/js/serverstatus.js
+++ web/js/serverstatus.js
@@ -89,7 +89,6 @@ function uptime() {
 						"<td id=\"ip_status\"><div class=\"progress\"><div style=\"width: 100%;\" class=\"progress-bar progress-bar-warning\"><small>加载中</small></div></div></td>" +
 						"<td id=\"name\">加载中</td>" +
 						"<td id=\"type\">加载中</td>" +
-						"<td id=\"host\">加载中</td>" +
 						"<td id=\"location\">加载中</td>" +
 						"<td id=\"uptime\">加载中</td>" +
 						"<td id=\"load\">加载中</td>" +
@@ -148,9 +147,6 @@ function uptime() {
 
 			// Type
 			TableRow.children["type"].innerHTML = result.servers[i].type;
-
-			// Host
-			TableRow.children["host"].innerHTML = result.servers[i].host;
 
 			// Location
 			TableRow.children["location"].innerHTML = result.servers[i].location;
